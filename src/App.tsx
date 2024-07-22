316557894c9576418c80f97457f63991b73b9e31
Muhammd Yousaf <yousafva9@gmail.com>
2024-07-22 02:49:44 -0700

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 
 import './App.css'
-import Footer from './components/footer/footer'
 import SessionFour from './components/Sessionfour/SessionFour'
 import Sessiontwo from './components/sessiontwo/Sessiontwo'
 import Web from './components/web'
@@ -15,7 +14,6 @@ function App() {
       <Sessiontwo/>
       <SessionThree/>
       <SessionFour/>
-    
     </>
   )
 }
